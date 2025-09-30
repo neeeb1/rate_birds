dabe0844893d575e7b77532626bcc861b53ee6f5
neeeb1 <18238012+neeeb1@users.noreply.github.com>
2025-09-29 23:15:44 -0700

    Add basic go file server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/neeeb1/rate_birds
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := http.Server{}
+
+	server.Handler = mux
+	server.Addr = ":8080"
+
+	server.ListenAndServe()
+
+}
