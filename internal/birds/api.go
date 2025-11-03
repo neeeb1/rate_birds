465f972b207761d9e74883d28ff3031e3dee3b46
neeeb1 <18238012+neeeb1@users.noreply.github.com>
2025-11-03 07:05:56 -0800

    Add htmx payload with values

--- internal/birds/api.go
+++ internal/birds/api.go
@@ -8,7 +8,7 @@ import (
 func RegisterEndpoints(mux *http.ServeMux, cfg *ApiConfig) {
 	mux.Handle("/", http.FileServer(http.Dir(".")))
 	mux.HandleFunc("GET /htmx/birds/random", cfg.handleGetRandomBird)
-
+	mux.HandleFunc("POST /api/ratings/score_match", cfg.handleScoreMatch)
 }
 
 func (cfg *ApiConfig) handleGetRandomBird(w http.ResponseWriter, r *http.Request) {
@@ -19,8 +19,41 @@ func (cfg *ApiConfig) handleGetRandomBird(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	type resp struct {
+		Side   string `json:"side"`
+		BirdID string `json:"birdID"`
+	}
+
+	var res resp
+	side := r.URL.Query().Get("side")
+	fmt.Println(side)
+	res.Side = side
+
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	payload := fmt.Sprintf("<p>%s</p>\n<p><em>%s</em></p>", rng_bird[0].CommonName.String, rng_bird[0].ScientificName.String)
+	payload := fmt.Sprintf(
+		`<div class="card" id="%s-bird">
+        	<img class="card-image" src="assets/American_Barn_Owl,_Bear_River,_Utah_(9637780911).jpg">
+        	<div class="card-text">
+				<p>%s</p>
+				<p><em>%s</em></p>
+				<Button hx-get="/htmx/birds/random"
+					hx-trigger="click"
+					hx-target="#%s-bird"
+					hx-swap="outerHTML"
+					hx-vals='{"side": "%s"}'>
+					This one!
+				</Button>
+			</div>
+		</div>`,
+		res.Side,
+		rng_bird[0].CommonName.String,
+		rng_bird[0].ScientificName.String,
+		res.Side,
+		res.Side)
 
 	w.Write([]byte(payload))
+}
+
+func (cfg *ApiConfig) handleScoreMatch(w http.ResponseWriter, r *http.Request) {
+
 }
